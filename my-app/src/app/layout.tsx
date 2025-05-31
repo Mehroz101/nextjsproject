f17f86d05a7dcafb444e1a3e70010d460bf2c33d
Mehroz farooq <mehrozfarooq127@gmail.com>
2025-05-31 14:29:08 +0500

    create new next js setup

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,13 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import './lib/fontawesome';
-import { Poppins } from 'next/font/google';
-const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-poppins', // Optional: use as CSS variable
-});
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -31,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${poppins.variable} antialiased `}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
